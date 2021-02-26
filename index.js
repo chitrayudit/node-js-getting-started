75d975afb4e4a1b5f35e657faa93023f7eb1aa32
chitrayudit <udit.chitray@adglobal360.com>
2021-02-26 17:19:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const PORT = process.env.PORT || 5000
 //   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  var app=express()
-    app.use(express.static(path.join(__dirname, 'public')))
-    app.set('views', path.join(__dirname, 'views'))
-    app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
-  app.get('/api', (req,res) => res.send('Hello World'))
-  app.post('/api-post', (req, res) => res.send('POST request to the homepage'))
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app=express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/api', (req,res) => res.send('Hello World'))
+app.post('/api-post', (req, res) => res.send(funcReturn()))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function funcReturn(){
+  return ({'some':'text'});
+}
